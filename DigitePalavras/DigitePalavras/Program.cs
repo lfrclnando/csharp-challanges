e316b6541ea974f7db6ec0060272fee6e3aa95c8
Fernando <31930791+lfrclnando@users.noreply.github.com>
2025-09-27 16:38:58 -0300

    Feat: word typing challenge

---
+++ DigitePalavras/DigitePalavras/Program.cs
@@ -0,0 +1,24 @@
+﻿string tracosPalavras = new string('-', 10);
+string tracosResultado = new string('-', 16);
+
+Console.WriteLine($"{tracosPalavras} DIGITA AS PALAVRAS {tracosPalavras}\n");
+
+Console.Write("Digite uma ou mais palavras: ");
+string wordsOriginal = (Console.ReadLine());
+
+if (string.IsNullOrWhiteSpace(wordsOriginal))
+{
+    Console.WriteLine("\nVocê não digitou nada... Tente novamente!!!");
+    return;
+}
+
+string wordsWithoutSpaces = wordsOriginal.Replace(" ", "");
+
+int count = wordsWithoutSpaces.Length;
+
+Console.WriteLine($"\n{tracosResultado} RESULTADO {tracosResultado}\n");
+
+Console.WriteLine($"Texto original: \"{wordsOriginal}\"");
+Console.WriteLine($"Texto sem espaços: \"{wordsWithoutSpaces}\"");
+Console.WriteLine($"\nTotal de caracteres (sem os espaços): {count}\n");
+Console.WriteLine("-------------------------------------------");
