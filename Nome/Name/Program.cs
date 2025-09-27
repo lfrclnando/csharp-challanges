8794d9ff064bbaf6c28d48638421c73e301a6a98
Fernando <31930791+lfrclnando@users.noreply.github.com>
2025-09-27 16:41:08 -0300

    Feat: names challenge

---
+++ Nome/Name/Program.cs
@@ -0,0 +1,22 @@
+﻿string tracosName = new string('-', 10);
+Console.WriteLine($"{tracosName} BEM VINDO(A) {tracosName}\n");
+
+Console.Write("Escreva seu nome: ");
+
+string name = Console.ReadLine();
+
+Console.Write($"\nOlá, {name}! Seja bem-vindo(a)!\n");
+
+
+// ---------------------------------------------------------
+
+string tracosSurname = new string('-', 10);
+Console.WriteLine($"\n{tracosSurname} NOME & SOBRENOME {tracosSurname}\n");
+
+Console.Write("Escreva seu nome: ");
+string firstName = Console.ReadLine();
+
+Console.Write("Escreva seu sobrenome: ");
+string surname = Console.ReadLine();
+
+Console.Write($"\nOlá {firstName} {surname}, tudo bem?!\n");
